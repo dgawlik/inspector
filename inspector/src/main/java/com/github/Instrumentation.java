a5f48a198dc128cc6d99772d71590431d9e30f33
= <=>
2024-11-03 16:36:37 +0100

    fix instrumentation

--- inspector/src/main/java/com/github/Instrumentation.java
+++ inspector/src/main/java/com/github/Instrumentation.java
@@ -8,11 +8,11 @@ import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
 import java.lang.reflect.Proxy;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.function.Consumer;
 
 public class Instrumentation {
@@ -86,45 +86,66 @@ public class Instrumentation {
         }
     }
 
+    public void instrumentRecurse(ApplicationContext ctx,
+                                  Map<String, List<String>> mappings,
+                                  Consumer<CallResult> callback,
+                                  Map<String, List<String>> graph,
+                                  Set<String> visited,
+                                  String parent,
+                                  String current){
+        if(visited.contains(current)){
+            return;
+        }
+
+        visited.add(current);
+
+        if(graph.containsKey(current)){
+            for(var child : graph.get(current)){
+                instrumentRecurse(ctx, mappings, callback, graph, visited, current, child);
+            }
+        }
+
+        try {
+            beanFactory(ctx).autowireBean(ctx.getBean(current));
+            var beanName = current;
+            var methodNames = mappings.get(beanName);
+
+            if(methodNames != null) {
+                var bean = instrument(ctx, callback, beanName, methodNames);
+
+                beanFactory(ctx).removeBeanDefinition(beanName);
+                beanFactory(ctx).registerSingleton(beanName, bean);
+
+                if (parent != null) {
+                    beanFactory(ctx).autowireBean(beanFactory(ctx).getBean(parent));
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
     public void instrumentAll(ApplicationContext ctx, Map<String, List<String>> mappings, Consumer<CallResult> callback) {
 
-        for (var entry : mappings.entrySet()) {
+        Map<String, List<String>> dependencyGraph = new HashMap<>();
 
-            String beanName = entry.getKey();
-            Object bean = ctx.getBean(beanName);
-            List<String> methodNames = entry.getValue();
+        for (String beanName : mappings.keySet()) {
+            dependencyGraph.put(beanName, Arrays.asList(beanFactory(ctx).getDependenciesForBean(beanName)));
+        }
 
-            try {
-                int modifiers = bean.getClass().getModifiers();
-                boolean isFinal = Modifier.isFinal(modifiers);
-
-                if (bean.getClass().getInterfaces().length > 0) {
-                    bean = Proxy.newProxyInstance(
-                            bean.getClass().getClassLoader(),
-                            bean.getClass().getInterfaces(),
-                            new InvocationHandler(bean, beanName, methodNames, callback)
-                    );
-                } else if (!isFinal) {
-                    bean = new ByteBuddy()
-                            .subclass(ctx.getType(beanName))
-                            .method(ElementMatchers.any()).intercept(MethodDelegation.to(new Interceptor(beanName, methodNames, callback)))
-                            .make()
-                            .load(this.getClass().getClassLoader())
-                            .getLoaded()
-                            .getDeclaredConstructor()
-                            .newInstance();
-
-                } else {
-                    throw new RuntimeException("Cannot instrument final class");
-                }
-
-
-                beanFactory(ctx).removeBeanDefinition(entry.getKey());
-                beanFactory(ctx).registerSingleton(entry.getKey(), bean);
-
-            } catch (Exception e) {
-                throw new RuntimeException(e);
+        for (var otherBean : beanFactory(ctx).getBeanDefinitionNames()) {
+            var deps = Arrays.asList(beanFactory(ctx).getDependenciesForBean(otherBean));
+            if (deps.stream().anyMatch(mappings::containsKey)) {
+                dependencyGraph.put(otherBean, deps.stream().filter(mappings::containsKey).toList());
             }
+        }
+
+
+        var visited = new HashSet<String>();
+        for (var key : dependencyGraph.keySet()) {
+            instrumentRecurse(ctx, mappings, callback, dependencyGraph, visited, null, key);
         }
 
         beanFactory(ctx).getBeanNamesIterator().forEachRemaining(otherBean -> {
@@ -133,6 +154,34 @@ public class Instrumentation {
                 beanFactory(ctx).autowireBean(other);
             }
         });
+    }
+
+    private Object instrument(ApplicationContext ctx, Consumer<CallResult> callback, String beanName, List<String> methodNames) throws InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+        Object bean = ctx.getBean(beanName);
+
+        int modifiers = bean.getClass().getModifiers();
+        boolean isFinal = Modifier.isFinal(modifiers);
+
+        if (bean.getClass().getInterfaces().length > 0) {
+            bean = Proxy.newProxyInstance(
+                    bean.getClass().getClassLoader(),
+                    bean.getClass().getInterfaces(),
+                    new InvocationHandler(bean, beanName, methodNames, callback)
+            );
+        } else if (!isFinal) {
+            bean = new ByteBuddy()
+                    .subclass(ctx.getType(beanName))
+                    .method(ElementMatchers.any()).intercept(MethodDelegation.to(new Interceptor(beanName, methodNames, callback)))
+                    .make()
+                    .load(this.getClass().getClassLoader())
+                    .getLoaded()
+                    .getDeclaredConstructor()
+                    .newInstance();
+
+        } else {
+            throw new RuntimeException("Cannot instrument final class");
+        }
+        return bean;
     }
 
     private DefaultListableBeanFactory beanFactory(ApplicationContext ctx) {
